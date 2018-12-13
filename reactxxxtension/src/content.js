fae80e37ea6d393dec9aa59d12be813c445fa878
Brent Schroder <i-brent@edmodo.com>
2018-12-12 16:50:46 -0800

    fixed post eject problems with react extension

---
+++ reactxxxtension/src/content.js
@@ -0,0 +1,18 @@
+import React { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "./content.css";
+
+class Main extends Component {
+    render() {
+        return (
+            <div className={'my-extension'}>
+                <h1>Hello world - My first Extension</h1>
+            </div>
+        )
+    }
+}
+
+const app = document.createElement('div');
+app.id = "my-extension-root";
+document.body.appendChild(app);
+ReactDOM.render(<Main />, app);
